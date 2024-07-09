6903b93f595e35393ae8dfc1c180c35d58831ab9
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2024-07-10 05:27:05 +0530

    finallizing

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.routes.js"
@@ -7,15 +8,22 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
+const __dirname = path.resolve();
+
 app.use(cors())
 app.use(express.json());
 
 app.use("/api/users",userRoutes)
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
 app.use("/",(req,res)=>{
     res.send("Hello World")
 })
 
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 app.listen(port,()=>{
     connectDB()
     console.log(`Server is connected to PORT: ${port}`)
